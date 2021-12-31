c16cc63c080aefd67701153ea95b02abdc9a7587
ryantd <xiaoyu.zhai@hotmail.com>
2021-12-31 15:28:51 +0800

    Remove tf callback

--- phetware/callback/train.py
+++
@@ -1,7 +0,0 @@
-from tensorflow.keras.callbacks import Callback
-import ray.train as train
-
-
-class TFTrainReportCallback(Callback):
-    def on_epoch_end(self, _, logs=None):
-        train.report(**logs)
