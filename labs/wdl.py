c248f174b0bfde0769b37af4e576bf9e305e53f4
ryantd <xiaoyu.zhai@hotmail.com>
2022-01-06 14:25:39 +0800

    WDL example update

--- labs/wdl.py
+++ labs/wdl.py
@@ -68,7 +68,7 @@ def train_wdl_dist(num_workers=2, use_gpu=False):
 
     trainer = Trainer("torch", num_workers=num_workers, use_gpu=use_gpu)
     trainer.start()
-    
+
     results = trainer.run(
         train_func=WideAndDeep(),
         dataset=datasets,
@@ -76,24 +76,27 @@ def train_wdl_dist(num_workers=2, use_gpu=False):
         # support fault tolerance with checkpoints
         # checkpoint=dict(),
         config={
+            # API: defined in WideAndDeep model
             "dnn_feature_columns": feature_columns["dnn"],
             "linear_feature_columns": feature_columns["linear"],
-            "epochs": 10,
-            "batch_size": 256,
-            "dnn_dropout": 0.2,
             "seed": RAND_SEED,
-            # support multiple loss functions with fixed weight
-            "loss_fn": LossFnStack(
-                dict(fn=nn.BCELoss(), weight=0.2),
-                dict(fn=nn.HingeEmbeddingLoss(), weight=0.8)),
-            # support multiple optimizers
-            "optimizer": OptimizerStack(
-                dict(cls=torch.optim.Adagrad, model_key="deep_model"),
-                dict(cls=FTRL, args=dict(alpha=1.0, beta=1.0, l1=1.0, l2=1.0), model_key="wide_model")),
-            # support torchmetrics and sklearn metric funcs
-            "metric_fns": [torchmetrics.AUROC(), log_loss],
             "output_fn": torch.softmax,
             "output_fn_args": dict(dim=0),
+            "dnn_dropout": 0.2,
+            # API: defined in train lifecycle
+            "epochs": 10,
+            "batch_size": 256,
+            "loss_fn": LossFnStack(
+                # support multiple loss functions with fixed weight
+                dict(fn=nn.BCELoss(), weight=0.2),
+                dict(fn=nn.HingeEmbeddingLoss(), weight=0.8)),
+            "optimizer": OptimizerStack(
+                # support multiple optimizers
+                dict(cls=torch.optim.Adagrad, model_key="deep_model"),
+                dict(cls=FTRL, args=dict(alpha=1.0, beta=1.0, l1=1.0, l2=1.0), model_key="wide_model")),
+            "metric_fns": [
+                # support torchmetrics and sklearn metric funcs
+                torchmetrics.AUROC(), log_loss],
             "torch_dataset_options": dict(
                 label_column="label",
                 feature_columns=sparse_features + dense_features,
