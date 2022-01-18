2429de64a715f32c05a832a89ff0c7cecd1ee4c3
ryantd <xiaoyu.zhai@hotmail.com>
2022-01-18 22:35:11 +0800

    Minor fix

--- phetware/util.py
+++ phetware/util.py
@@ -5,11 +5,13 @@ TIME_DIFF = "epoch_seconds"
 
 class StyleCoder(object):
     HEADER = "\033[95m"
-    OKBLUE = "\033[94m"
-    OKCYAN = "\033[96m"
-    OKGREEN = "\033[92m"
+    BLUE = "\033[94m"
+    CYAN = "\033[96m"
+    GREEN = "\033[92m"
+    BOLDGREEN = "\033[1;92m"
     WARNING = "\033[93m"
-    FAIL = "\033[91m"
+    RED = "\033[91m"
+    BOLDRED = "\033[1;91m"
     ENDC = "\033[0m"
     BOLD = "\033[1m"
     UNDERLINE = "\033[4m"
@@ -84,14 +86,14 @@ def pprint_results(run_results, use_style=True, print_interval=1):
                     f"[epoch {idx + 1}/{total}: {'%.3f' % time_diff}s]\t{metrics_join}"
                 )
             acc_metrics_join = "\t".join([
-                f"{s.OKGREEN}{k}: {'%.3f' % -(v*100)}%{s.ENDC}"
+                f"{k}: {s.BOLDGREEN}↓ {s.ENDC}{'%.3f' % (v*100)}%"
                 if v > 0
-                else f"{s.FAIL}{k}: +{'%.3f' % -(v*100)}%{s.ENDC}"
+                else f"{k}: {s.BOLDRED}↑ {s.ENDC}{'%.3f' % -(v*100)}%"
                 for k, v in acc_metrics.items()
             ])
             print(
                 f"{s.BOLD}================="
                 f"\nWorker {worker_idx} analysis"
                 f"\n================={s.ENDC}"
-                f"\n{acc_metrics_join}\n"
+                f"\n[epoch 1→{total}]\t{acc_metrics_join}\n"
             )
